a94129bedb7c3522a0bf03be7e8c79a4f0224a99
BenjiCoder02 <benkoshy02@gmail.com>
2021-08-01 18:32:20 -0400

    added more items and fixed carousel btns

--- amazon/client/src/Home.js
+++ amazon/client/src/Home.js
@@ -24,6 +24,18 @@ function Home() {
           <Item imgSrc="http://pngimg.com/uploads/frisbee/frisbee_PNG63.png" price="6.99" name="Frisbee"/>
           <Item imgSrc="https://e7.pngegg.com/pngimages/674/742/png-clipart-iphone-x-front-iphone-x.png" price="699" name="iPhone 7"/>
           <Item imgSrc="https://cdn.vox-cdn.com/thumbor/Vgy3FfpWvBD32CYZrcNq6itGqnw=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/20034840/ishMfuW.png" price="1999" name="PS5"/>
+          <Item imgSrc="https://www.pngfind.com/pngs/m/129-1290136_iphone-8-plus-apple-iphone-8-plus-hd.png" price="699" name="iPhone 7"/>
+          <Item imgSrc="http://pngimg.com/uploads/frisbee/frisbee_PNG63.png" price="6.99" name="Frisbee"/>
+          <Item imgSrc="https://e7.pngegg.com/pngimages/674/742/png-clipart-iphone-x-front-iphone-x.png" price="699" name="iPhone 7"/>
+          <Item imgSrc="https://cdn.vox-cdn.com/thumbor/Vgy3FfpWvBD32CYZrcNq6itGqnw=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/20034840/ishMfuW.png" price="1999" name="PS5"/>
+          <Item imgSrc="https://www.pngfind.com/pngs/m/129-1290136_iphone-8-plus-apple-iphone-8-plus-hd.png" price="699" name="iPhone 7"/>
+          <Item imgSrc="http://pngimg.com/uploads/frisbee/frisbee_PNG63.png" price="6.99" name="Frisbee"/>
+          <Item imgSrc="https://e7.pngegg.com/pngimages/674/742/png-clipart-iphone-x-front-iphone-x.png" price="699" name="iPhone 7"/>
+          <Item imgSrc="https://cdn.vox-cdn.com/thumbor/Vgy3FfpWvBD32CYZrcNq6itGqnw=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/20034840/ishMfuW.png" price="1999" name="PS5"/>
+          <Item imgSrc="https://www.pngfind.com/pngs/m/129-1290136_iphone-8-plus-apple-iphone-8-plus-hd.png" price="699" name="iPhone 7"/>
+          <Item imgSrc="http://pngimg.com/uploads/frisbee/frisbee_PNG63.png" price="6.99" name="Frisbee"/>
+          <Item imgSrc="https://e7.pngegg.com/pngimages/674/742/png-clipart-iphone-x-front-iphone-x.png" price="699" name="iPhone 7"/>
+          <Item imgSrc="https://cdn.vox-cdn.com/thumbor/Vgy3FfpWvBD32CYZrcNq6itGqnw=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/20034840/ishMfuW.png" price="1999" name="PS5"/>
          
          </div>
       
--- amazon/client/src/components/Navbar.jsx
+++ amazon/client/src/components/Navbar.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from 'react'
 import "./component_styles/Navbar.css"
 import SearchIcon from '@material-ui/icons/Search';
-
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 
 
 const Navbar = () => {
+    
    
+
+   const [cartCount, setCartCount] = useState(0)
     return (
         <div className="navbar">
             <img src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" style={{height: '35px'}} alt="amazon logo"/>
             <div className="input" >
                 <input type="text" />
+               
                 <button type='submit'><SearchIcon /></button>
+               
             </div>
             <a href="#">Hello, Sign in</a>
             <a href="#" className="returns">Returns & Orders</a>
-            <a href='#' className="cart">Cart</a>
+            <a href='#' className="cart"><ShoppingCartIcon /> <p> {cartCount}</p></a>
             
         </div>
     )
