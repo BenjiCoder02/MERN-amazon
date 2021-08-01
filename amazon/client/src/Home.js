daec1786860d2306656ba284a046b9d64f5cd60c
BenjiCoder02 <benkoshy02@gmail.com>
2021-08-01 16:15:32 -0400

    added carousel for home page

--- amazon/client/src/Home.js
+++ amazon/client/src/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import Item from "./components/Item"
+import Carousel from "./components/Carousel";
 import "./App.css"
 
 function Home() {
@@ -17,6 +18,7 @@ function Home() {
       <div className="home">
 
          {/*<p>{!data ? "Loading..." : data}</p> */}
+         <Carousel />
          <div className="item-container">
           <Item imgSrc="https://www.pngfind.com/pngs/m/129-1290136_iphone-8-plus-apple-iphone-8-plus-hd.png" price="699" name="iPhone 7"/>
           <Item imgSrc="http://pngimg.com/uploads/frisbee/frisbee_PNG63.png" price="6.99" name="Frisbee"/>
@@ -24,6 +26,7 @@ function Home() {
           <Item imgSrc="https://cdn.vox-cdn.com/thumbor/Vgy3FfpWvBD32CYZrcNq6itGqnw=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/20034840/ishMfuW.png" price="1999" name="PS5"/>
          
          </div>
+      
         
   
       </div>
